410232de11a8c2b6bfd8b237c6c75893bd9c180d
AutoExtractor <crgarcia@live.com.ar>
2021-02-26 09:42:46 +0100

    init function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# az-aks-audit-function
-Auditing Azure with Functions
+# TODO
+This is still a blank project. More to come
+
+# How to link this to event grid?
+https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-event-grid-trigger?tabs=csharp%2Cbash
--- src/UdrChangeAudit.cs
+++ src/UdrChangeAudit.cs
@@ -1,11 +1,8 @@
-// Default URL for triggering event grid function in the local environment.
-// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace src
 {
@@ -14,7 +11,20 @@ namespace src
         [FunctionName("UdrChangeAudit")]
         public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
         {
+            // delay time
             log.LogInformation(eventGridEvent.Data.ToString());
+
+
+            dynamic eventData = JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+
+            if (eventGridEvent.EventType == "Microsoft.Resources.ResourceWriteSuccess" 
+                && eventData["operationName"] == "Microsoft.Network/routeTables/routes/write"
+                //&& eventGridEvent.Subject.StartsWith("/subscriptions/930c11b0-5e6d-458f-b9e3-f3dda0734110/resourceGroups/crgar-dns-es-spoke/providers/Microsoft.Network/routeTables/crgar-dns-es-spoke-udr/routes")
+            )
+            {
+                log.LogInformation("Route table has been changed");
+                
+            }
         }
     }
 }
